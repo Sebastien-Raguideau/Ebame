4008e01286da8a9c34edc662a7392f2a42ab8b5b
Seb <idaho90@live.fr>
2019-10-25 15:58:48 +0100

    fix install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # get repos
 mkdir -p $HOME/repos
-git clone https://github.com/Sebastien-Raguideau/Ebame19-Quince.git $HOME/repos/
+git clone https://github.com/Sebastien-Raguideau/Ebame19-Quince.git $HOME/repos/Ebame19-Quince
 
 # conda install everything else
 conda env create -f $HOME/repos/Ebame19-Quince/conda_env.yaml 
