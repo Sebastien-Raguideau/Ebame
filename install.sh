0e29a8ec9b6fd2de9e4c80283d6119b5fbd5d89a
Seb <idaho90@live.fr>
2019-10-25 14:38:30 +0100

    simplified install

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# conda install 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p $HOME/miniconda
-
 # get repos
 mkdir -p $HOME/repos
 git clone https://github.com/Sebastien-Raguideau/Ebame19-Quince.git $HOME/repos/
