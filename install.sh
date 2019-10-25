1485f27d08aaa192a43e21acf5e3ff6f3aa31fb8
Seb <idaho90@live.fr>
2019-10-25 13:53:51 +0100

    should work

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 
-# conda install everything else
-mkdir $HOME/repos
+# get repos
+mkdir -p $HOME/repos
+git clone https://github.com/Sebastien-Raguideau/Ebame19-Quince.git $HOME/repos/
 
-conda env create -f conda_env.yaml
+# conda install everything else
+conda env create -f $HOME/repos/Ebame19-Quince/conda_env.yaml 
+
