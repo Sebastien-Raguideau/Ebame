230d86337039a426e44615376a57a8e0e886db04
Seb <idaho90@live.fr>
2019-10-28 00:58:08 +0100

    bad HOME variable

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-# get repos 
-mkdir -p $HOME/repos
-git clone https://github.com/Sebastien-Raguideau/EBAME19-MetaHood.git $HOME/repos/Ebame19-Quince/
+# define home
+export HOME2=/user/ubuntu
+
+# ----- get all repos ----- 
+mkdir -p $HOME2/repos
+cd $HOME2/repos
+
+git clone https://github.com/Sebastien-Raguideau/Ebame19-Quince.git
+git clone https://github.com/Sebastien-Raguideau/Metahood.git
+git clone https://github.com/chrisquince/Ebame5.git
+git clone https://github.com/BinPro/CONCOCT.git
+git clone https://github.com/chrisquince/DESMAN.git
+git clone https://github.com/chrisquince/MAGAnalysis.git
 
 # conda install 
 source /etc/profile.d/conda.sh 
@@ -26,7 +36,17 @@ chown -R 1000:1000 /var/lib/miniconda3/*
 sed -i 's/original_data.values()/original_data.values/g' /var/lib/miniconda3/envs/MetaHood/bin/concoct_refine 
 
 # get rpsblast+
-apt install ncbi-blast+
+apt install ncbi-blast+ --assume-yes
+
+# ----- COG databases ------
+mkdir ~/Databases
+cd ~/Databases
+wget https://desmandatabases.s3.climb.ac.uk/rpsblast_cog_db.tar.gz
+tar -xvzf rpsblast_cog_db.tar.gz
+
+# Make Metahood run with old cod db....
+cp CONCOCT/scgs/cdd_to_cog.tsv Metahood/scg_data/cdd_to_cog.tsv
+
 
 # ---- install R libraries ----
 # add repo serverkey
@@ -54,19 +74,26 @@ sudo $APP_DIR/scripts/RPInstall.sh plyr
 sudo $APP_DIR/scripts/RPInstall.sh grid
 sudo $APP_DIR/scripts/RPInstall.sh gridExtra
 
+# -----------install guppy --------------
+cd $HOME2/repos
+wget https://mirror.oxfordnanoportal.com/software/analysis/ont-guppy-cpu_3.3.0_linux64.tar.gz
+tar -xvzf ont-guppy-cpu_3.3.0_linux64.tar.gz 
+
+export PATH=~/ont-guppy-cpu/bin:$PATH
+
 # ---------- modify .bashrc ------------------
 # add -h to ll 
-sed -i "s/alias ll='ls -alF'/alias ll='ls -alhF'/g" $HOME/.bashrc 
+sed -i "s/alias ll='ls -alF'/alias ll='ls -alhF'/g" $HOME2/.bashrc 
 
 # add multitude of export to .bashrc
-echo -e "\n\n #------ export path to repos/db -------">>$HOME/.bashrc 
-echo "export CONCOCT=~/repos/CONCOCT">>$HOME/.bashrc 
-echo "export DESMAN=~/repos/DESMAN">>$HOME/.bashrc 
-echo "export EBAME5=~/repos/Ebame5">>$HOME/.bashrc 
-echo "export MAGAnalysis=~/repos/MAGAnalysis">>$HOME/.bashrc 
-echo "export COGSDB_DIR=~/Databases/rpsblast_cog_db">>$HOME/.bashrc 
+echo -e "\n\n #------ export path to repos/db -------">>$HOME2/.bashrc 
+echo "export CONCOCT=~/repos/CONCOCT">>$HOME2/.bashrc 
+echo "export DESMAN=~/repos/DESMAN">>$HOME2/.bashrc 
+echo "export EBAME5=~/repos/Ebame5">>$HOME2/.bashrc 
+echo "export MAGAnalysis=~/repos/MAGAnalysis">>$HOME2/.bashrc 
+echo "export COGSDB_DIR=~/Databases/rpsblast_cog_db">>$HOME2/.bashrc 
 
 # to be able to launch conda 
-echo -e "\n\n #------ necessary to use conda -------">>$HOME/.bashrc 
-echo -e "source /etc/profile.d/conda.sh ">>$HOME/.bashrc
+echo -e "\n\n #------ necessary to use conda -------">>$HOME2/.bashrc 
+echo -e "source /etc/profile.d/conda.sh ">>$HOME2/.bashrc
 
