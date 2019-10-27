2c7b377fdf3ecec95ef8da93de11d54584ff1406
Seb <idaho90@live.fr>
2019-10-27 14:01:34 +0000

    merge 2 commit

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# get repos 
+mkdir -p $HOME/repos
+git clone https://github.com/Sebastien-Raguideau/EBAME19-MetaHood.git $HOME/repos/EBAME19-MetaHood/
+
 # conda install 
 source /etc/profile.d/conda.sh 
 
@@ -21,17 +25,25 @@ chown -R 1000:1000 /var/lib/miniconda3/*
 # fix concoct install, so that concoct_refine works
 sed -i 's/original_data.values()/original_data.values/g' /var/lib/miniconda3/envs/MetaHood/bin/concoct_refine 
 
+# add -h to ll 
+sed -i "s/alias ll='ls -alF'/alias ll='ls -alhF'/g" .bashrc 
 
+
+# ---- install R libraries ----
+# add repo serverkey
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 
+# add repo 
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
 
+# add look for update from repo
 sudo apt-get update
 
+# install updates
 sudo apt-get install --assume-yes evince
-
 sudo apt-get install --assume-yes r-base r-base-dev
 
+# install R packages
 sudo $APP_DIR/scripts/RPInstall.sh ggplot2
 sudo $APP_DIR/scripts/RPInstall.sh reshape
 sudo $APP_DIR/scripts/RPInstall.sh reshape2
@@ -42,3 +54,4 @@ sudo $APP_DIR/scripts/RPInstall.sh ellipse
 sudo $APP_DIR/scripts/RPInstall.sh plyr
 sudo $APP_DIR/scripts/RPInstall.sh grid
 sudo $APP_DIR/scripts/RPInstall.sh gridExtra
+
