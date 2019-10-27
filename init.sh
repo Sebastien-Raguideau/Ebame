2e8850677d340a6709355c5547cc3f6f91e5f398
chrisquince <c.quince@warwick.ac.uk>
2019-10-27 09:38:43 +0000

    Update

--- init.sh
+++ init.sh
@@ -26,3 +26,9 @@ wget https://desmandatabases.s3.climb.ac.uk/rpsblast_cog_db.tar.gz
 tar -xvzf rpsblast_cog_db.tar.gz
 
 export COGSDB_DIR=~/Databases/rpsblast_cog_db
+
+wget https://mirror.oxfordnanoportal.com/software/analysis/ont-guppy-cpu_3.3.0_linux64.tar.gz
+
+tar -xvzf ont-guppy-cpu_3.3.0_linux64.tar.gz 
+
+export PATH=~/ont-guppy-cpu/bin:$PATH
