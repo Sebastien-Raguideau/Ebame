0bccb73f06946708194ca1b7e8099dd7fe01f717
Sebastien-Raguideau <idaho90@live.fr>
2021-10-27 15:56:47 +0100

    test fast start

--- install.sh
+++ install.sh
@@ -1,49 +1,51 @@
 #!/usr/bin/env bash
 
-# define home
-export HOME2=/home/ubuntu
+echo "works"
 
-# ----- get all repos ----- 
-mkdir -p $HOME2/repos
-cd $HOME2/repos
+# # define home
+# export HOME2=/home/ubuntu
 
-git clone https://github.com/Sebastien-Raguideau/Ebame21-Quince.git
-git clone --recurse-submodules https://github.com/chrisquince/STRONG.git
-cd STRONG
-git submodule foreach git pull origin master
+# # ----- get all repos ----- 
+# mkdir -p $HOME2/repos
+# cd $HOME2/repos
 
-# ----- run strong install --------
-sudo apt-get update
-sudo apt-get -y install libbz2-dev libreadline-dev cmake g++ zlib1g zlib1g-dev
-bash install_STRONG.sh 
+# git clone https://github.com/Sebastien-Raguideau/Ebame21-Quince.git
+# git clone --recurse-submodules https://github.com/chrisquince/STRONG.git
+# cd STRONG
+# git submodule foreach git pull origin master
 
-
-# -----------Rob env --------------
-cd $HOME2/repos
-
-# --- guppy ---
-wget https://europe.oxfordnanoportal.com/software/analysis/ont-guppy-cpu_5.0.16_linux64.tar.gz
-tar -xvzf ont-guppy-cpu_5.0.16_linux64.tar.gz 
-# guppy install
-echo -e "\n\n #------ guppy path -------">>$HOME2/.bashrc 
-echo -e "export PATH=~/repos/ont-guppy-cpu/bin:$PATH ">>$HOME2/.bashrc
-
-# --- everything else ---
-mamba install $APP_DIR/conda_env_LongReads.yaml
+# # ----- run strong install --------
+# sudo apt-get update
+# sudo apt-get -y install libbz2-dev libreadline-dev cmake g++ zlib1g zlib1g-dev
+# bash install_STRONG.sh 
 
 
-# ---------- modify .bashrc ------------------
-# add -h to ll 
-sed -i "s/alias ll='ls -alF'/alias ll='ls -alhF'/g" $HOME2/.bashrc 
+# # -----------Rob env --------------
+# cd $HOME2/repos
 
-# add multitude of export to .bashrc
-echo -e "\n\n #------ export path to repos/db -------">>$HOME2/.bashrc
+# # --- guppy ---
+# wget https://europe.oxfordnanoportal.com/software/analysis/ont-guppy-cpu_5.0.16_linux64.tar.gz
+# tar -xvzf ont-guppy-cpu_5.0.16_linux64.tar.gz 
+# # guppy install
+# echo -e "\n\n #------ guppy path -------">>$HOME2/.bashrc 
+# echo -e "export PATH=~/repos/ont-guppy-cpu/bin:$PATH ">>$HOME2/.bashrc
+
+# # --- everything else ---
+# mamba install $APP_DIR/conda_env_LongReads.yaml
+
+
+# # ---------- modify .bashrc ------------------
+# # add -h to ll 
+# sed -i "s/alias ll='ls -alF'/alias ll='ls -alhF'/g" $HOME2/.bashrc 
+
+# # add multitude of export to .bashrc
+# echo -e "\n\n #------ export path to repos/db -------">>$HOME2/.bashrc
 
 
 
-# ------------ fix rigths --------------------
-# fix conda ownership, so that user can create stuffs
-chown -R 1000:1000 /var/lib/miniconda3/*
+# # ------------ fix rigths --------------------
+# # fix conda ownership, so that user can create stuffs
+# chown -R 1000:1000 /var/lib/miniconda3/*
 
-# fix HOME2 ownership, so that user can create stuffs
-chown -R 1000:1000 $HOME2/*
+# # fix HOME2 ownership, so that user can create stuffs
+# chown -R 1000:1000 $HOME2/*
