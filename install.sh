dd57f6aa4dcf670d1f5f1e1d28d0c1b7aeb9359f
Sebastien-Raguideau <idaho90@live.fr>
2021-10-29 09:59:40 +0100

    non interactive session

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ sudo apt-get update
 # STRONG compilation
 sudo apt-get -y install libbz2-dev libreadline-dev cmake g++ zlib1g zlib1g-dev
 # bandage and utils
-sudo apt-get install qt5-default gzip feh evince
+sudo apt-get -y install qt5-default gzip feh evince
 
 # ------------------------------
 # ----- Chris tuto -------------
@@ -35,7 +35,7 @@ export PATH=/var/lib/miniconda3/condabin:$PATH
 # Bandage install
 cd $HOME2/repos/
 wget https://github.com/rrwick/Bandage/releases/download/v0.8.1/Bandage_Ubuntu_dynamic_v0_8_1.zip
-mkdir Bandage && unzip  Bandage_Ubuntu_dynamic_v0_8_1.zip -d Bandage && mv Bandage_Ubuntu_dynamic_v0_8_1.zip Bandage
+mkdir Bandage && unzip Bandage_Ubuntu_dynamic_v0_8_1.zip -d Bandage && mv Bandage_Ubuntu_dynamic_v0_8_1.zip Bandage
 
 # trait inference
 mamba env create -f $APP_DIR/conda_env_Trait_inference.yaml
@@ -46,13 +46,14 @@ mamba env create -f $APP_DIR/conda_env_Trait_inference.yaml
 # --- guppy ---
 cd $HOME2/repos
 wget https://europe.oxfordnanoportal.com/software/analysis/ont-guppy-cpu_5.0.16_linux64.tar.gz
-tar -xvzf ont-guppy-cpu_5.0.16_linux64.tar.gz && mv ont-guppy-cpu_5.0.16_linux64.tar.gz guppy
+tar -xvzf ont-guppy-cpu_5.0.16_linux64.tar.gz && mv ont-guppy-cpu_5.0.16_linux64.tar.gz ont-guppy-cpu/
 
 # --- everything else ---
 mamba env create -f $APP_DIR/conda_env_LongReads.yaml
 
 # --- Pavian ---
-R -e 'if (!require(remotes)) { install.packages("remotes") }
+conda activate LongReads
+R -e 'if (!require(remotes)) { install.packages("remotes",repos="https://cran.irsn.fr") }
 remotes::install_github("fbreitwieser/pavian")'
 
 # -------------------------------------
